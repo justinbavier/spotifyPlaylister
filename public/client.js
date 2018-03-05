99b43b653658420b577042ac4ca3ef939e9f6af3
justinbaver <jbavier123@gmail.com>
2018-03-05 15:05:44 -0600

    Initial commit

---
+++ public/client.js
@@ -0,0 +1,53 @@
+// Check hash for token
+const hash = window.location.hash
+  .substring(1)
+  .split('&')
+  .reduce(function(initial, item) {
+    if (item) {
+      var parts = item.split('=');
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
+window.location.hash = '';
+
+// Set token
+let _token = hash.access_token;
+
+const authEndpoint = 'https://accounts.spotify.com/authorize';
+const clientId = 'b8f7c75be8df4476bbd74e05fe622524';
+const redirectUri = 'http://localhost:5000';
+const scopes = [
+  'streaming',
+  'user-read-birthdate',
+  'user-read-email',
+  'user-read-private',
+  'playlist-modify-public',
+  'user-modify-playback-state'
+];
+
+// If there is no token, redirect to Spotify Authorization
+if (!_token) {
+  window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+    '%20'
+  )}&response_type=token`;
+}
+
+// Page Setup
+showUser();
+
+function showUser() {
+  $.get('/user?token=' + _token, function(user) {
+    $('#current-user').text(user.id);
+    $('#display-name').text(user.display_name);
+  });
+}
+
+// Need to make this a more streamlined process
+// Currently opens logout page in new tab
+// Logout only truly happens upon manual refresh
+function logout() {
+  _token = null;
+  window.open('https://accounts.spotify.com/logout');
+  location.reload();
+}
---
+++ server.js
@@ -0,0 +1,92 @@
+// Introducing the Node Modules
+var express = require('express');
+var request = require('request');
+var querystring = require('querystring');
+
+var app = express();
+
+const spotifyBaseUrl = 'https://api.spotify.com/v1/';
+
+app.use(express.static(__dirname + '/public'));
+
+// Get our user
+app.get('/user', function(req, res) {
+  let token = req.query.token;
+
+  let requestUrl = spotifyBaseUrl + 'me';
+
+  let options = {
+    url: requestUrl,
+    headers: { Authorization: 'Bearer ' + token },
+    json: true
+  };
+
+  request.get(options, function(error, response, body) {
+    res.json(body);
+  });
+});
+
+// List all available genres for recommendation seeds
+app.get('/genres', function(req, res) {
+  let token = req.query.token;
+  let requestUrl = spotifyBaseUrl + 'recommendations/available-genre-seeds';
+
+  let options = {
+    url: requestUrl,
+    headers: { Authorization: 'Bearer ' + token },
+    json: true
+  };
+
+  request.get(options, function(error, response, body) {
+    res.json(body.genres);
+  });
+});
+
+// Make that sucker a playlist
+app.post('/playlist', function(req, res) {
+  let token = req.query.token;
+  let userId, playlistUrl;
+
+  // Get User Id
+  let requestUrl = spotifyBaseUrl + 'me';
+
+  let options = {
+    url: requestUrl,
+    headers: { Authorization: 'Bearer ' + token },
+    json: true
+  };
+
+  request.get(options, function(error, response, body) {
+    userId = body.id;
+    displayName = body.display_name;
+    console.log(body);
+    console.log('Creating playlist for ' + displayName);
+
+    // Create Playlist
+    requestUrl = spotifyBaseUrl + 'users/' + userId + '/playlists';
+
+    options = {
+      url: requestUrl,
+      headers: {
+        Authorization: 'Bearer ' + token,
+        'Content-Type': 'application/json'
+      },
+      json: true,
+      dataType: 'json',
+      body: {
+        name: 'Playlist just for ' + displayName + '!!!',
+        description: "It's your playlist!"
+      }
+    };
+
+    request.post(options, function(error, response, body) {
+      playlistUrl = body.tracks.href;
+      res.sendStatus(200);
+    });
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+
+console.log('It lives on port ' + PORT + '!');

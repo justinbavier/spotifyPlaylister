0fc6c42d38892498c3ed8406693f63d71654a4b1
justinbaver <jbavier123@gmail.com>
2018-03-08 16:50:44 -0600

    Prototype

--- public/main.js
+++ public/main.js
@@ -16,8 +16,8 @@ let _token = hash.access_token;
 
 const authEndpoint = 'https://accounts.spotify.com/authorize';
 const clientId = 'b8f7c75be8df4476bbd74e05fe622524';
-// const redirectUri = 'http://crosshair-playlist.herokuapp.com';
-const redirectUri = 'http://localhost:5000';
+const redirectUri = 'http://crosshair-playlist.herokuapp.com';
+// const redirectUri = 'http://localhost:5000';
 const scopes = [
   'streaming',
   'user-read-birthdate',

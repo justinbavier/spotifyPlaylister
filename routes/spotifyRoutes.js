e874aa7811f362319ccedd8cbc97538a9e7cf061
justinbaver <jbavier123@gmail.com>
2018-03-15 23:02:50 -0500

    Could it be?

--- routes/spotifyRoutes.js
+++ routes/spotifyRoutes.js
@@ -196,6 +196,8 @@ module.exports = app => {
     let userId;
     let playlistUrl = 'https://api.spotify.com/v1/users/bavier123/playlists/3lQ94EvUZ5eockX8VJ1Zom'
 
+
+
     // let playlistUrl = req.query.playlistUrl;
 
     // 3. Add tracks to playlist

52bae15c3e71dbe1f052bdf1ae293f049670c070
justinbaver <jbavier123@gmail.com>
2018-03-15 21:41:03 -0500

    Could it be?

--- public/main.js
+++ public/main.js
@@ -16,8 +16,8 @@ let _token = hash.access_token;
 
 const authEndpoint = 'https://accounts.spotify.com/authorize';
 const clientId = 'b8f7c75be8df4476bbd74e05fe622524';
-const redirectUri = 'http://crosshair-playlist.herokuapp.com';
-// const redirectUri = 'http://localhost:5000';
+//const redirectUri = 'http://crosshair-playlist.herokuapp.com';
+const redirectUri = 'http://localhost:5000';
 const scopes = [
   'streaming',
   'user-read-birthdate',
@@ -258,6 +258,7 @@ function setUpSliders() {
   });
 
   $('#tempo-slider').slider({
+    orientation: 'vertical',
     min: 0,
     max: 200,
     step: 1,
